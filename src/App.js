2f99d07c3f101b000323ab2b6e3e2665f96833c2
JesusMezaOrozco <jesus.orozco@3dves.com>
2023-02-04 12:52:09 -0500

    :rocket: First Version
    I have finished the first verison

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
+import { useEffect, useState, useRef } from 'react'
 import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai'
 import { HiPlayPause } from 'react-icons/hi2'
 import { BiReset } from 'react-icons/bi'
-import { useState } from 'react'
-import moment from 'moment'
+import Countdown from 'react-countdown'
 
 import './App.css'
 
 function App() {
 	const [breakLength, setBreakLength] = useState(5)
 	const [sessionLength, setSessionLength] = useState(25)
-	const [timeLeft, setTimeLeft] = useState(0)
+	const [timeLeft, setTimeLeft] = useState(25000)
+	const [startPause, setStartPause] = useState(false)
+
+	const clockRef = useRef()
+
+	const increaseBreakLength = () => {
+		if (breakLength < 59) setBreakLength((breakLength) => breakLength + 1)
+	}
+	const decreaseBreakLength = () => {
+		if (breakLength > 0) setBreakLength((breakLength) => breakLength - 1)
+	}
+
+	const increaseSessionLength = () => {
+		if (sessionLength < 59)
+			setSessionLength((sessionLength) => sessionLength + 1)
+	}
+	const decreaseSessioniLength = () => {
+		if (sessionLength > 0)
+			setSessionLength((sessionLength) => sessionLength - 1)
+	}
+	const handleReset = () => {
+		setStartPause(!startPause)
+		setBreakLength(5)
+		setSessionLength(25)
+		if (startPause) {
+			clockRef.current.start()
+		} else {
+			setTimeLeft(timeLeft)
+			clockRef.current.pause()
+		}
+	}
+
+	useEffect(() => {
+		setTimeLeft(sessionLength * 1000)
+	}, [sessionLength])
+
+	useEffect(() => {
+		if (startPause) {
+			clockRef.current.start()
+		} else {
+			clockRef.current.pause()
+		}
+	}, [startPause])
+
 	return (
 		<div className='global_container'>
 			<div>
@@ -17,26 +60,44 @@ function App() {
 				<div id='setting_clock'>
 					<div className='setting_break_container'>
 						<label id='break-label'>Break Length</label>
-						<AiOutlineArrowDown id='break-decrement' />
+						<AiOutlineArrowDown
+							id='break-decrement'
+							className='button_action'
+							onClick={decreaseBreakLength}
+						/>
 						<div id='break-length'>{breakLength}</div>
-						<AiOutlineArrowUp id='break-increment' />
+						<AiOutlineArrowUp
+							id='break-increment'
+							className='button_action'
+							onClick={increaseBreakLength}
+						/>
 					</div>
 					<div className='setting_session_container'>
 						<label id='session-label'>Session Length</label>
-						<AiOutlineArrowDown id='session-decrement' />
+						<AiOutlineArrowDown
+							id='session-decrement'
+							className='button_action'
+							onClick={decreaseSessioniLength}
+						/>
 						<div id='session-length'>{sessionLength}</div>
-						<AiOutlineArrowUp id='session-increment' />
+						<AiOutlineArrowUp
+							id='session-increment'
+							className='button_action'
+							onClick={increaseSessionLength}
+						/>
 					</div>
 					<div className='session_state_display_container'>
 						<label id='timer-label'>Session</label>
-						<div id='time-left'>{moment(timeLeft * 1000).format('mm:ss')}</div>
+						<div id='time-left'>
+							<Countdown
+								ref={clockRef}
+								date={Date.now() + timeLeft * 60}
+							></Countdown>
+						</div>
 					</div>
 					<div className='session_control_container'>
-						<div id='start_stop'>
-							<HiPlayPause />
-						</div>
 						<div id='reset'>
-							<BiReset />
+							<BiReset className='button_action' onClick={handleReset} />
 						</div>
 					</div>
 				</div>
